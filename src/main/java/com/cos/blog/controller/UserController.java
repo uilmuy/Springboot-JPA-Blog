fc309a2d337f382c425912e013c06048f45808cf
uilmuy <uilmuy@gmail.com>
2022-05-23 12:38:31 +0900

    시큐리티 라이브러리의 접근 권한을 사용하기 위해 경로 변경

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -3,15 +3,18 @@ package com.cos.blog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//인증이 안된 사용자들이 출입 할 수 있는 경로를 /ayth 라는 경로 이하로만 허용 하겠다
+//그냥 주소가 "/"이면 index.jsp 허용
+//static 이하에 있는 것들 허용.js . img 등
 @Controller
 public class UserController {
 
-	@GetMapping("/user/joinForm")
+	@GetMapping("/auth/joinForm")
 	public String joinForm() {
 		return "user/joinForm";
 	}
 	
-	@GetMapping("/user/loginForm")
+	@GetMapping("/auth/loginForm")
 	public String loginForm() {	
 		return "user/loginForm";
 	}
